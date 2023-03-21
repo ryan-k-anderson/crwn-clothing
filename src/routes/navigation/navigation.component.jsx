9dfbdd398d3892a2d756f738d3737a5ba0a127f4
Ryan Kristopher Anderson <78626914+ryan-k-anderson@users.noreply.github.com>
2023-03-21 11:07:01 -0400

    Navigation Template Added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,18 @@
-import {Outlet} from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import{ Fragment } from 'react'
 
 const Navigation = () => {
     return(
     <Fragment>
         <div className='navigation'>
-            <h1>I am the navigation bar</h1>
+            <Link className='logo-container'>
+                <div>Logo</div>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className= 'nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
         </div>
         <Outlet />
     </Fragment>
